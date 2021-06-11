6427940978a2c1b66b05f3f9e3e7b18d520ec5f9
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-06-10 21:14:29 -0600

    Step 5 complete

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,9 +1,42 @@
 import React, {Component} from 'react';
 
 class FilterString extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      names: ['Arthur', 'Ford', 'Slartibartfast', 'Zaphod'],
+      userInput: '',
+      filteredNames: []
+    }
+  }
+
+  handleChange(val){
+    this.setState({userInput: val});
+  }
+
+  filteredResults(userInput){
+    let names = this.state.names;
+    let filteredNames = [];
+    
+    names.forEach(name => {
+      if(name.includes(userInput)){
+        filteredNames.push(name)
+      }
+    })
+
+    this.setState({filteredNames: filteredNames})
+  }
+
   render(){
     return(
-      <p>FilterString Component</p>
+      <div className="puzzleBox filterStringPB">
+        <h4>Filter String</h4>
+        <span className="puzzleText">Unfiltered: {JSON.stringify(this.state.names, null, 10)}</span>
+        <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
+        <button className="confirmationButton" onClick={() => this.filteredResults(this.state.userInput)}>Filter</button>
+        <span className="resultsBox filterStringRB">Filtered: {JSON.stringify(this.state.filteredNames, null, 10)}</span>
+      </div>
     )
   }
 }
