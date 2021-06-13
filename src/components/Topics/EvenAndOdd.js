0cbc2a87b2c838aad23cef1e2d1a9773722f7e70
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-06-12 21:20:43 -0600

    fixed input className for EvenAndOdd

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -48,7 +48,7 @@ class EvenAndOdd extends Component{
     return(
       <div className="puzzleBox evenAndOddPB">
         <h4>Evens and Odds</h4>
-        <input className="inputline" onChange={(e) => this.handleChange(e.target.value)}/>
+        <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}/>
         <button className='confirmationButton' onClick={() => {this.assignEvenAndOdds(this.state.userInput) }}>Split</button>
         <button className='confirmationButton' onClick={() => this.setState({userInput: '1,2,3,4,5,6,7,8,9,10'})}>Ex: 1-10</button>
         <span className="resultsBox">Evens: {JSON.stringify(this.state.evenArray, null, 1)}</span>
